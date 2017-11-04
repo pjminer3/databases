2ccd715d1bd07755a0aeb2f6d857fac3accfe123
pjminer3 <patrickminer318@gmail.com>
2017-11-03 19:16:47 -0700

    Created working database and tables

--- server/schema.sql
+++ server/schema.sql
@@ -1,13 +1,45 @@
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
-USE chat;
+USE chat; 
 
 CREATE TABLE messages (
+  id INTEGER AUTO_INCREMENT,
+  message CHAR(250) NOT NULL,
+  createdAt TIMESTAMP NOT NULL,
+  updatedAt TIMESTAMP NULL DEFAULT NULL,
+  id_ROOMS INTEGER NOT NULL,
+  id_USERS INTEGER NOT NULL,
+  PRIMARY KEY (id) 
   /* Describe your table here.*/
 );
 
+CREATE TABLE users (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  userName CHAR(25) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE rooms (
+  id integer auto_increment,
+  roomName char(25) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+-- CREATE TABLE users_rooms (
+--   id INTEGER NOT NULL AUTO_INCREMENT,
+--   id_USERS INTEGER NOT NULL,
+--   id_ROOMS INTEGER NOT NULL,
+--   PRIMARY KEY ('id');
+-- );
+
+
 /* Create other tables and define schemas for them here! */
 
+ALTER TABLE messages ADD FOREIGN KEY (id_ROOMS) REFERENCES rooms(id);
+ALTER TABLE messages ADD FOREIGN KEY (id_USERS) REFERENCES users(id);
+-- ALTER TABLE `users_rooms` ADD FOREIGN KEY (id_USERS) REFERENCES `users` (`id`);
+-- ALTER TABLE `users_rooms` ADD FOREIGN KEY (id_ROOMS) REFERENCES `rooms` (`id`);
 
 
 
