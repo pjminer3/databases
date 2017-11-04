07505b66953566f27c445ab721d5bed46934d037
pjminer3 <patrickminer318@gmail.com>
2017-11-04 14:44:21 -0700

    basic functionality for db

--- server/db/index.js
+++ server/db/index.js
@@ -3,5 +3,16 @@ var mysql = require('mysql');
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
+// THE ABOVE IS WRONG!
+
+var dbConnection = mysql.createConnection({
+  user: 'student',
+  password: 'student',
+  database: 'chat'
+});
+
+
+
+module.exports = dbConnection;
 
 
--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,102 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function () {
+      // open conneciton
+      db.connect(function(err) {
+        if (err) {
+          console.log('There was an error connect to Database: messages GET');
+        } else {
+          console.log('Connected!');
+        }
+      });
+      
+      // query database
+      db.query('SELECT * FROM messages', function(err, rows, fields) {
+        if (err) {
+          console.log('There was an error querying the database');
+        } else {
+          console.log('Query successful');
+        }
+      });
+      
+      //close connection
+      db.end();
+      
+    }, // a function which produces all the messages
+    post: function (messageObj) {
+      // open conneciton
+      db.connect(function(err) {
+        err ? console.log('There was an error connect to Database: messages POST') : console.log('Connected!');
+      });
+      
+      // Insert message into message table with message.text, createdAt value, userID and roomID
+      db.query('INSERT INTO messages (message, createdAt, id_ROOMS, id_USERS) VALUES (?, ?, ?, ?)', [messageObj.text, messageObj.createdAt, 1, 1], function(err, row, fields) {
+        if (err) { // add ??? []
+          console.log('There was an error POSTing to the database: ', err);
+        } else {
+          console.log('New message POST successful');
+        }
+      });
+      db.end();
+      
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
     get: function () {},
-    post: function () {}
+    post: function (userName) {
+      // open conneciton
+      db.connect(function(err) {
+        err ? console.log('There was an error connect to Database: messages POST') : console.log('Connected!');
+      });
+      
+      // Insert user into users table with roomname
+      db.query('INSERT INTO users (userName) VALUES (?)', [userName], function(err, row, fields) {
+        if (err) { 
+          console.log('There was an error POSTing a new user to the database: ', err);
+        } else {
+          console.log('New user POST successful');
+        }
+      });
+      
+      //close connection
+      db.end();
+    }
+  }, 
+  
+  rooms: {
+    get: function() {},
+    post: function(roomName) {
+      // open conneciton
+      db.connect(function(err) {
+        err ? console.log('There was an error connect to Database: messages POST') : console.log('Connected!');
+      });
+      
+      // Insert room into rooms table with roomname
+      db.query('INSERT INTO rooms (roomName) VALUES (?)', [roomName], function(err, row, fields) {
+        if (err) { // add ??? []
+          console.log('There was an error POSTing a new room to the database: ', err);
+        } else {
+          console.log('New room POST successful');
+        }
+      });
+      
+      //close connection
+      db.end();
+    }
   }
 };
+
+// POST NEW ROOM
+// module.exports.rooms.post('lobby'); // SUCCESS
+
+// POST NEW USER
+// module.exports.users.post('Bernard'); // SUCCESS
+
+/// QUERY WAS SUCCESSFUL
+// module.exports.messages.post({text: 'hey', createdAt: 'now'}); // SUCCESS
+
+
 
